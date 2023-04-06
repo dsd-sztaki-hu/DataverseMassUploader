b524103812ae53d96cbc17cd4a96329fc50467d0
Pallinger Péter <pallinger@dsd.sztaki.hu>
2023-04-06 18:06:22 +0200

    * set directoryLabel (dataverse folder) for files * minor refactoring

--- uploadFilesToExistingDataset.py
+++ uploadFilesToExistingDataset.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import os
+import json
 import time
 import shutil
 import argparse
@@ -20,27 +21,37 @@ args = argparser.parse_args()
 
 curlAvailable=subprocess.run(["curl --version"], shell=True, capture_output=True).returncode == 0
 
-api = NativeApi(args.baseUrl,args.apiKey)
-for filename in args.infile:
-	#print("File size: %d"%os.lstat(filename).st_size)
-	filesize=os.lstat(filename).st_size
-	if filesize>=2**31:
-		if(not curlAvailable):
-			print("ERROR: Not uploading %s: size is too big (%d bytes), but curl is unavailable."%(filename,filesize))
-			continue
-		print("Size of %s is too big (%d bytes), uploading with curl."%(filename,filesize))
-		response=subprocess.run('curl -H "X-Dataverse-key:%s" -X POST -F "file=@%s" -F "jsonData={}" "%s/api/datasets/:persistentId/add?persistentId=%s"'%(
-				args.apiKey,filename,args.baseUrl,args.dataset), shell=True, capture_output=True)
-		if response.returncode != 0:
-			print("Error uploading %s"%filename)
-			print("  Response message was: '%s'"%response.stdout)
-		else:
-			print("Successful upload: %s"%filename)
+def uploadWithCurl(filename,fileMetadata):
+	print("Size of %s is too big (%d bytes), uploading with curl."%(filename,filesize))
+	response=subprocess.run(
+			'curl -H "X-Dataverse-key:%s" -X POST -F "file=@%s" -F "jsonData=%s" "%s/api/datasets/:persistentId/add?persistentId=%s"'%(
+				args.apiKey,filename,fileMetadata,args.baseUrl,args.dataset),
+			shell=True, capture_output=True)
+	if response.returncode != 0:
+		print("Error uploading %s"%filename)
+		print("  Response message was: '%s'"%response.stdout)
 	else:
-		response=api.upload_datafile(args.dataset, filename, json_str=None, is_pid=True)
+		print("Successful upload: %s"%filename)
+
+def upload(filename,fileMetadata):
+		response=api.upload_datafile(args.dataset, filename, json_str=fileMetadata, is_pid=True)
 		if response.status_code!=200:
 			print("Error uploading %s"%filename)
 			print("  Response message was: '%s'"%response._content)
 #			var_dump(response)
 		else:
 			print("Successful upload: %s"%filename)
+
+
+api = NativeApi(args.baseUrl,args.apiKey)
+for filename in args.infile:
+	filesize=os.lstat(filename).st_size
+	fileMetadata=json.dumps({"filename": filename, "directoryLabel": os.path.dirname(filename)})
+	#print("File size: %d"%filesize)
+	if filesize>=2**31:
+		if(not curlAvailable):
+			print("ERROR: Not uploading %s: size is too big (%d bytes), but curl is unavailable."%(filename,filesize))
+			continue
+		uploadWithCurl(filename,fileMetadata)
+	else:
+		upload(filename,fileMetadata)

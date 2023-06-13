d222b12d0fd3fee8b7871c78e79e3399d82c03a8
Pallinger Péter <pallinger@dsd.sztaki.hu>
2023-06-13 15:48:10 +0200

    * fix upload bug for small files by sleeping 0.1 sec * add description and tag support

--- uploadFilesToExistingDataset.py
+++ uploadFilesToExistingDataset.py
@@ -6,15 +6,21 @@ import time
 import shutil
 import argparse
 import subprocess
+from functools import partial
 from pyDataverse.api import NativeApi, DataAccessApi, SearchApi
 from pyDataverse.models import Dataverse, Dataset
 from var_dump import var_dump
+
+_csv = partial(str.split, sep=',')
 
 argparser = argparse.ArgumentParser(description ='Mass upload files to existing dataset', argument_default=[])
 argparser.add_argument('-d', '--dataset', dest ='dataset', help='dataset pid to upload to')
 argparser.add_argument('-u', '--dataverseBaseUrl', dest ='baseUrl', help='dataverse base URL to upload to')
 argparser.add_argument('-k', '--apiKey', dest ='apiKey', help='dataverse API key/token')
 argparser.add_argument('-v', '--verbose', dest ='verbosity', action='count', help='verbosity level', default=0)
+argparser.add_argument('--description', dest ='description', help='file description to set for all files')
+argparser.add_argument('--tags', dest ='tags', type=_csv, help='Comma delimited list of file tags (aka categories) to set for all files. Allowed values are usually: Documentation, Data, Code; however, custom tags are usually allowed')
+#argparser.add_argument('--tabularTags', dest ='tabularTags', type=_csv, help='Comma delimited list of tabular file tags to set for all files. Allowed values are usually: Geospatial, Time Series, Event, Network, Panel, Genomics, Survey')
 argparser.add_argument("infile", nargs='+', help='the name of the file(s) to upload')
 args = argparser.parse_args()
 
@@ -22,6 +28,7 @@ args = argparser.parse_args()
 
 curlAvailable=subprocess.run(["curl --version"], shell=True, capture_output=True).returncode == 0
 curlUseThreashold=int(os.environ.get('DataverseMassUploaderCurlUseThreshold', 2**25))
+api = NativeApi(args.baseUrl,args.apiKey)
 
 def vp(level: int, str1: str, obj=None):
 	if args.verbosity >= level:
@@ -53,12 +60,16 @@ def upload(filename,fileMetadata):
 		print("Successful upload: %s"%filename)
 		vp(1,"  Response message was: '%s'"%response._content)
 
-api = NativeApi(args.baseUrl,args.apiKey)
+
 for filename in args.infile:
+	time.sleep(0.1) # there seems to be a bug in dataverse if files are uploaded to the same dataverse too rapidly. A 0.1s delay seems to fix it -- mostly...
 	filesize=os.lstat(filename).st_size
 	directoryLabel=os.path.dirname(filename)
 	fileMetadata={"filename": filename}
 	if directoryLabel: fileMetadata["directoryLabel"]=directoryLabel
+	if args.description: fileMetadata["description"]=args.description
+	if args.tags: fileMetadata["categories"]=args.tags
+#	if args.tabularTags: fileMetadata["tabularTags"]=args.tabularTags
 	fileMetadata=json.dumps(fileMetadata)
 	vp(1,"File size: %d"%filesize)
 	if filesize>=curlUseThreashold:
